6fa239e36585faa55ed3d16ad601093ed9ca83a1
raj8888 <jadhavrj8877@gmail.com>
2022-12-14 20:22:26 +0530

    fw22_0388 -- admin page --

---
+++ admin_page.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+//   let maincont=document.querySelector(".maincontainer")
+  
+  
+//   function upload_Data(){
+//     maincont.innerHTML=""
+//     maincont.innerHTML=`
+     
+//     `
+//   }
