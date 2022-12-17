619c03b3c43937902eaaf1b2badffea316a2b444
ank501 <ankitsyadav246@gmail.com>
2022-12-17 23:10:29 +0530

    drop down , search ,login and signup , profile icon and job page created

---
+++ job.js
@@ -0,0 +1,8 @@
+document.getElementById("logout").addEventListener("click",()=>{
+    window.location.href="index.html"
+})
+
+
+document.getElementById("profile").addEventListener("click",()=>{
+    window.location.href="Profile.html"
+})
